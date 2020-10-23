cfdfe031febc84dd23b1e9af17d6920fc3b153dd
hanyue.ph <hanyue.ph@cainiao.com>
2020-10-23 16:01:36 +0800

    restaurant

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,7 +1,60 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
+//     }
+// }
+
+// use front_of_house::hosting;
+
+// pub fn eat_at_restaurant() {
+//     // Absolute path
+//     crate::front_of_house::hosting::add_to_waitlist();
+
+//     // Relative path
+//     front_of_house::hosting::add_to_waitlist();
+
+//     // use Use
+//     hosting::add_to_waitlist();
+// }
+
+// mod back_of_house {
+//     pub struct Breakfast {
+//         pub toast: String,
+//         seasonal_fruit: String,
+//     }
+
+//     impl Breakfast {
+//         pub fn summer(toast: &str) -> Breakfast {
+//             Breakfast {
+//                 toast: String::from(toast),
+//                 seasonal_fruit: String::from("peaches"),
+//             }
+//         }
+//     }
+// }
+
+// pub fn eat_at_restaurant() {
+//     // Order a breakfast in the summer with Rye toast
+//     let mut meal = back_of_house::Breakfast::summer("Rye");
+//     // Change our mind about what bread we'd like
+//     meal.toast = String::from("Wheat");
+//     println!("I'd like {} toast please", meal.toast);
+
+//     // The next line won't compile if we uncomment it; we're not allowed
+//     // to see or modify the seasonal fruit that comes with the meal
+//     // meal.seasonal_fruit = String::from("blueberries");
+// }
+
+mod front_of_house;
+mod test_a_mod;
+
+pub use crate::front_of_house::hosting;
+pub use crate::test_a_mod::test_mod;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+
+    test_mod::test_fn();
 }
--- rust_enum/src/main.rs
+++ rust_enum/src/main.rs
@@ -1,3 +1,94 @@
-fn main() {
-    println!("Hello, world!");
+// #[derive(Debug)]
+// enum IpAddrKind {
+//     V4,
+//     V6,
+// }
+
+// #[derive(Debug)]
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
+
+// #[derive(Debug)]
+// enum IpAddr {
+//     V4(u8, u8, u8, u8),
+//     V6(String),
+// }
+
+// impl IpAddr {
+//     fn say (&self) {
+//         println!("{:#?}", &self);
+//     }
+// }
+
+// fn main() {
+//     let home = IpAddr::V4(127, 0, 0, 1);
+//     let loopback = IpAddr::V6(String::from("::1"));
+//     home.say();
+//     loopback.say();
+// }
+
+// fn main () {
+//     let num_0 = 5;
+//     let num_1 = Option::Some(5);
+//     let num_2 = Option::Some(10);
+//     let str_1 = Option::Some("a string");
+//     let absent_number: Option<i32> = Option::None;
+    
+//     println!("{}", num_1.unwrap() + num_0);
+//     println!("{}", num_1.unwrap() + num_2.unwrap());
+//     println!("{}", num_1.unwrap() + absent_number.unwrap());
+// }
+
+// fn main () {
+//     #[derive(Debug)] // so we can inspect the state in a minute
+//     enum UsState {
+//         Alabama,
+//         Alaska,
+//         // --snip--
+//     }
+
+//     enum Coin {
+//         Penny,
+//         Nickel,
+//         Dime,
+//         Quarter(UsState),
+//     }
+    
+//     fn value_in_cents(coin: Coin) -> u8 {
+//         match coin {
+//             Coin::Penny => 1,
+//             Coin::Nickel => 5,
+//             Coin::Dime => 10,
+//             Coin::Quarter(state) => {
+//                 match state {
+//                     Alabama => 200,
+//                     Alaska => 30
+//                 }
+//             },
+//         }
+//     }
+//     let coin_1 = Coin::Dime;
+//     let coin_2 = Coin::Quarter(UsState::Alabama);
+
+//     println!("{}", value_in_cents(coin_1)); // 10
+//     println!("{}", value_in_cents(coin_2)); // 200
+// }
+
+fn main () {
+    let num_0 = 5;
+    let num_1 = Option::Some(5);
+    let num_2 = Option::Some(10);
+    let str_1 = Option::Some("a string");
+    let absent_number: Option<i32> = Option::None;
+    
+    fn plus_one (num: Option<i32>) -> Option<i32> {
+        match num {
+            Some (v) => Some(v + 1),
+            None => None,
+        }
+    }
+    println!("{:#?}", plus_one(num_1));
+    println!("{:#?}", plus_one(absent_number));
 }
